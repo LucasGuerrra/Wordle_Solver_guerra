d777fb194905a03b5acf30058c4d2dc613797841
LucasGuerrra <159138922+LucasGuerrra@users.noreply.github.com>
2024-07-22 03:54:24 -0300

    Add files via upload

---
+++ wordleReverse.py
@@ -0,0 +1,46 @@
+import os
+
+def Search_Word ():
+    file = open('letters\z.txt', 'r')
+
+    #CRANE and SAUCY starting words
+    exists = ['E','B','R','A']
+    no_exists = ['C','D','F','G','H','I','J','K','L','M','N','O','P','Q','S','T','U','V','W','X','Y']
+    return_word = ''
+
+    for line in file:
+        word = line.strip()
+        fuck_off = False
+        digital_exists = exists.copy()
+        pontuation = 0
+        end = False
+
+        for character in word:
+            for not_letter in no_exists:
+                if not_letter == character:
+                    fuck_off = True
+                    print(word, ' fuck off')
+                    break
+            if fuck_off == True:
+                break
+
+            for yes_letter in digital_exists:
+                if yes_letter == character:
+                    pontuation += 1
+                    digital_exists.remove(yes_letter)
+
+            if pontuation == 5:
+                return_word = word
+                end = True
+                break
+        
+        if end == True:
+            break
+        if fuck_off == True:
+            continue
+        
+    file.close()
+    return return_word
+    
+# letter = input('What letter do you want to search?: ')
+print(Search_Word())
